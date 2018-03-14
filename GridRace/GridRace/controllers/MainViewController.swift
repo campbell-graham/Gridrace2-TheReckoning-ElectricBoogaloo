3ff2fc62496e520a37eb49931c4e74895d28e52e
Unknown <campbell.graham@gridstone.com.au>
2018-03-14 14:30:43 +1100

    Map layout margins set

--- GridRace/GridRace/controllers/MainViewController.swift
+++ GridRace/GridRace/controllers/MainViewController.swift
@@ -106,6 +106,7 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
     override func viewDidLayoutSubviews() {
         collectionView.collectionViewLayout = customFlowLayout(collectionViewWidth: collectionView.frame.width, collectionViewHeigth: collectionView.frame.height)
         collectionView.layoutIfNeeded()
+        mapView.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: collectionView.frame.height, right: 16)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
