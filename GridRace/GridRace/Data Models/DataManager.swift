16d9532f34770317395baaafea625eb2d2aadd82
Unknown <christian.vassallo@gridstone.com.au>
2018-03-26 15:09:23 +1100

    moved save/load functionality into dataManager

---
+++ GridRace/GridRace/Data Models/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  GridRace
+//
+//  Created by Christian on 3/26/18.
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+}
