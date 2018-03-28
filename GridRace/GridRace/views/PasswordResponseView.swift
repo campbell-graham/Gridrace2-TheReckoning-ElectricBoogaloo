a15c10374e015b5bb8c81031ef9262dc5529b086
Unknown <campbell.graham@gridstone.com.au>
2018-03-28 11:30:33 +1100

    PasswordViewController is now a view

---
+++ GridRace/GridRace/views/PasswordResponseView.swift
@@ -0,0 +1,48 @@
+//
+//  PasswordView().swift
+//  GridRace
+//
+//  Created by Christian on 3/6/18.
+//  Copyright © 2018 Gridstone. All rights reserved.
+//
+
+import UIKit
+
+class PasswordResponseView: UIView {
+
+    private let passcode = "1234"
+    private var attempt = ""
+    var textField = UITextField()
+
+    private var buttons = [UIButton]()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //text field set up
+        textField.keyboardType = .numberPad
+        textField.backgroundColor = AppColors.textPrimaryColor
+        textField.isSecureTextEntry = true
+        textField.placeholder = "Enter password..."
+        textField.textColor = UIColor.black
+        textField.layer.cornerRadius = 10
+        textField.textAlignment = .center
+        
+        //add items to view
+        addSubview(textField)
+        
+        //layout constraints
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            textField.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            textField.heightAnchor.constraint(equalToConstant: 30)
+            ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
