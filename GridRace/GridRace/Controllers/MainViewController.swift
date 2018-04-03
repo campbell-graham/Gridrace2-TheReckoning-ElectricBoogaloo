96c34839e3ddec4b699538c38babedf411947545
Unknown <campbell.graham@gridstone.com.au>
2018-04-03 13:04:35 +1000

    Bug fix with alert triggering for wrong objective

--- GridRace/GridRace/Controllers/MainViewController.swift
+++ GridRace/GridRace/Controllers/MainViewController.swift
@@ -469,7 +469,7 @@ class MainViewController: UIViewController, UICollectionViewDelegate, UICollecti
         }
         
         //if they've selected the last main objective (i.e. the final one with the PIN) then alert them to confirm they want to continue
-        if objectivesToDisplay[indexPath.row] == objectivesToDisplay.filter({$0.objectiveType == .main}).last {
+        if objectivesToDisplay[indexPath.row].objectiveType == .last {
             let refreshAlert = UIAlertController(title: "Continue?", message: "This is the end of the race, and once you finish you will be unable to get any more points. Are you sure you wish to continue?", preferredStyle: UIAlertControllerStyle.alert)
             
             refreshAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action: UIAlertAction!) in
